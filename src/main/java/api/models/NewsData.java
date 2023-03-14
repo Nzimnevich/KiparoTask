cf2c55ad26f5ca4b557b8abd8656276a270ff93a
PVD-Teacher <127093027+pvd-teacher@users.noreply.github.com>
2023-03-14 17:08:53 +0530

    Update NewsData.java

--- src/main/java/api/models/NewsData.java
+++ src/main/java/api/models/NewsData.java
@@ -26,6 +26,10 @@ public class NewsData {
     Boolean visible;
 
 
+    // Задумка понятна, но как вариант я бы предложил вынести эту чать из модели
+    // потому что, конвертация даты и сбор строки уже смахивает на бизнес логику, ведь возможно потребуется другой вариант вывода,
+    // а мы в модели
+    // лучше иметь какой-нибудь NewsPrintAdapter снаружи, который будет собирать вывод
     @Override
     public String toString() {
         return DataHelper.convertToBeautyData(date) + "\n" + "Название: " + title + "\n" + "Новость: " + description; //todo title может быть пустым
